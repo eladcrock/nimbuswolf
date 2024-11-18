75b2e95085b0d839ed667a1ba1b141cadf8c4753
Dale Crockwell <eladcrock@gmail.com>
2024-11-17 18:15:05 -0800

    Renamed app

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,35 @@ import '@mantine/core/styles.css';
 import { MantineProvider } from '@mantine/core';
 import { Router } from './Router';
 import { theme } from './theme';
+import { HeaderSimple } from './components/HeaderSimple';
+import '@mantine/core/styles.css';
+
+import { HeroImageBackground } from "./components/hero.tsx";
+import { FeaturesImages } from "./components/FeaturesImages.tsx";
+import { StatsGroup } from "./components/StatsGroup.tsx";
+import { CardsCarousel } from "./components/CardsCarousel.tsx";
+import { FooterSimple } from "./components/FooterSimple.tsx";
+import { ColorSchemeToggle } from './components/ColorSchemeToggle/ColorSchemeToggle';
+
 
 export default function App() {
   return (
+
     <MantineProvider theme={theme}>
+      <HeaderSimple />
       <Router />
+
+        <FeaturesImages />
+        <HeroImageBackground />
+ 
+        <CardsCarousel />
+
+
+        <StatsGroup />
+        <FooterSimple />
+
+
+
     </MantineProvider>
   );
 }
