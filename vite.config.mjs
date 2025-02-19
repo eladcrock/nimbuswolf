fef7c62d0108e5371faad3137e01e61472b0b0de
Dale Crockwell <eladcrock@gmail.com>
2025-02-19 15:14:07 -0800

    nav color link changed and some indexhtmls og tags

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,12 +1,29 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
+import ogPlugin from 'vite-plugin-open-graph';
 
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [
+    react(),
+    tsconfigPaths(),
+    ogPlugin({
+      basic: {
+        url: 'https://nimbuswolf.netlify.app', // The URL of your website
+        title: 'Nimbus Wolf Studios',
+        description: 'A web design and motion graphics studio crafting innovative solutions to drive business growth and success.',
+        image: '/images/laptop.jpg', // Place image in the 'public' directory
+        locale: 'en_US',
+        siteName: 'Nimbus Wolf Studios',
+      },
+      twitter: {
+        image: '/images/laptop.jpg', // Same image for Twitter
+      },
+    }),
+  ],
   test: {
     globals: true,
     environment: 'jsdom',
-    setupFiles: './vitest.setup.mjs',
+    setupFiles: './vitest.setup.mjs', // It's okay to keep the .mjs if you're using ESM imports
   },
 });
